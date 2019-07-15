2380ab96fbd49d3b94899d3a562abdcdb029a7d2
M. Shahrukh Khan <52627061+IMSRK09@users.noreply.github.com>
2019-07-15 06:24:33 +0500

    Add files via upload

---
+++ OouiTestXamlApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ooui;
+using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace OouiTestXamlApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forms.Init();
+            var page = new Page1();
+            UI.Publish("/", page.GetOouiElement());
+        }
+    }
+}
